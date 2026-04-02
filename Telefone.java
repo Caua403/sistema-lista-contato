8315f50aeff6a11227fdb0fef8d1534da33bc0eb
Caua403 <cauapradomartins@outlook.com>
2026-04-02 15:56:20 -0300

    Add files via upload

---
+++ Telefone.java
@@ -0,0 +1,26 @@
+package ListaContato;
+
+public class Telefone {
+	private String num;
+	private String ddd;
+	private String tipo;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
